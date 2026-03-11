6d67fca5952f9e85b6811c21019333d09f8f77ff
squidy320 <147177989+squidy320@users.noreply.github.com>
2026-03-12 00:36:37 +0200

    maybe fix

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -2,11 +2,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            Text("Kyomiru")
-                .font(.system(size: 32, weight: .bold))
-                .foregroundColor(.white)
-        }
+        RootView()
     }
 }
